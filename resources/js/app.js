4bc22a78e8101988b3d134f8f7e2dbf539d0e43b
Fabiolx26 <fabio.lx26@gmail.com>
2023-06-04 12:47:29 +0200

    feat: main: included fonts and installed vuetify

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,12 @@
 import './bootstrap';
 import { createApp } from 'vue';
+import Vuetify from 'vuetify';
 
 const app = createApp({});
 
 import ExampleComponent from './components/ExampleComponent.vue';
 app.component('example-component', ExampleComponent);
+
+app.use(Vuetify);
 
 app.mount('#app');
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,14 +3,16 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>Laravel</title>
         @vite('resources/js/app.js')
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
+        <link href="https://fonts.google.com/specimen/Raleway?selection.family=Raleway:400,600" />
+        <link href="https://fonts.google.com/specimen/Montserrat?query=montserrat&selection.family=Montserrat:400,700" />
+        <link href="https://fonts.google.com/specimen/Playfair+Display?category=Serif&selection.family=Playfair+Display:400,400i" />
+        <link href="https://fonts.google.com/specimen/Roboto+Condensed?selection.family=Roboto+Condensed:700" />
     </head>
     <body id="app">
         <example-component></example-component>
